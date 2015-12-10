8d680236984b2d92614ba0c28b40217df99a3581
aonamrata <aonamrata@gmail.com>
2015-12-10 21:08:58 +0530

    Fixed IE caching issue for AJAX

--- public_html/lightboardAPI.php
+++ public_html/lightboardAPI.php
@@ -8,15 +8,23 @@ try {
     if ($_POST) {
         $boardData = $board->postAction($_POST);
         header("Content-Type: text/json");
+        header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        header("Pragma: no-cache"); // HTTP 1.0.
+        header("Expires: 0"); // Proxies.
         echo $boardData;
     } else {
 
         $boardData = $board->indexAction();
+        header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        header("Pragma: no-cache"); // HTTP 1.0.
+        header("Expires: 0"); // Proxies.
         header("Content-Type: text/json");
         echo $boardData;
-
     }
 } catch (Exception $exec) {
     header("HTTP/1.1 500 Server Error");
+    header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+    header("Pragma: no-cache"); // HTTP 1.0.
+    header("Expires: 0"); // Proxies.
     echo json_encode(array("Error" => $exec->getMessage()));
 }
--- public_html/lockingAPI.php
+++ public_html/lockingAPI.php
@@ -8,22 +8,31 @@ try {
     if ($_POST && !empty($_POST['user_name'])) {
         $lockData = $lock->setLockStatus($_POST);
         header("Content-Type: text/json");
+        header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        header("Pragma: no-cache"); // HTTP 1.0.
+        header("Expires: 0"); // Proxies.
         echo $lockData;
+    } else if ($_POST && empty($_POST['user_name'])) {
 
-    }else if ($_POST && empty($_POST['user_name'])) {
-        
         $lockData = $lock->deleteLockStatus();
         header("Content-Type: text/json");
+        header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        header("Pragma: no-cache"); // HTTP 1.0.
+        header("Expires: 0"); // Proxies.
         echo $lockData;
-    
     } else {
 
         $lockData = $lock->getLockStatus();
         header("Content-Type: text/json");
+        header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        header("Pragma: no-cache"); // HTTP 1.0.
+        header("Expires: 0"); // Proxies.
         echo $lockData;
-
     }
 } catch (Exception $exec) {
     header("HTTP/1.1 500 Server Error");
+    header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
+    header("Pragma: no-cache"); // HTTP 1.0.
+    header("Expires: 0"); // Proxies.
     echo json_encode(array("Error" => $exec->getMessage()));
 }
